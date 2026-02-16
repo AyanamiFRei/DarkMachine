f4bf76c7a658a72251eb6ce380cf41fd325f8300
Sahar8-coder <63341153+Sahar8-coder@users.noreply.github.com>
2026-02-16 18:29:46 +0300

    Delete dmg_area.gd

--- dmg_area.gd
+++
@@ -1,6 +0,0 @@
-extends Node3D
-
-func _on_amg_area_3d_body_entered(body: Node3D) -> void:
-	if body.name == "Player" or body is CharacterBody3D:
-		if body.has_method("_on_death"):
-			body._on_death()
