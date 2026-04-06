e2b4e844a49e17ad6b856891838c2a5121c64444
Sahar8-coder <63341153+Sahar8-coder@users.noreply.github.com>
2026-04-06 14:58:05 +0300

    ХУЙ

---
+++ assets/objects/breakable_wall/breakable_wall.gd
@@ -0,0 +1,29 @@
+
+extends CharacterBody3D
+
+
+@onready var attack_healthbar_timer: Timer = $attack_healthbar_timer
+@onready var progress_bar = get_tree().get_nodes_in_group("healthbar")
+@onready var dmg_timer: Timer = $dmg_timer
+@onready var gg: CharacterBody3D = $"../player"
+var type = "enemy"
+
+
+var playerNode: CharacterBody3D
+var hp = 150
+
+var just_hit_player = false
+var target_point = CharacterBody3D
+
+func take_dmg(dmg):
+	hp -= dmg
+	#var mesh = $MeshInstance3D
+	#if mesh:
+		#
+	if hp <=0:
+		queue_free()
+
+
+#func _on_attack_healthbar_timer_timeout() -> void:
+	#if progress_bar.size() > 0:
+		#progress_bar[0].value -= 25
