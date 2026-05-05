3c8619abebb6c5982a8eafcf7d739cb75b52eb55
unknown <ladejnovmatvej@mail.ru>
2026-05-06 00:13:23 +0300

    Починил ломаемые стенки

--- assets/objects/breakable_wall/breakable_wall.gd
+++ assets/objects/breakable_wall/breakable_wall.gd
@@ -1,20 +1,7 @@
 
 extends CharacterBody3D
-
-
-@onready var attack_healthbar_timer: Timer = $attack_healthbar_timer
-@onready var progress_bar = get_tree().get_nodes_in_group("healthbar")
-@onready var dmg_timer: Timer = $dmg_timer
-
 var type = "enemy"
-
-
-var playerNode: CharacterBody3D
 var hp = 150
-
-var just_hit_player = false
-var target_point = CharacterBody3D
-
 func take_dmg(dmg):
 	hp -= dmg
 	#var mesh = $MeshInstance3D
@@ -22,8 +9,3 @@ func take_dmg(dmg):
 		#
 	if hp <=0:
 		queue_free()
-
-
-#func _on_attack_healthbar_timer_timeout() -> void:
-	#if progress_bar.size() > 0:
-		#progress_bar[0].value -= 25
