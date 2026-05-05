b1bfa16667d14cbcb8790de7910eb8a9e9418275
Sahar8-coder <63341153+Sahar8-coder@users.noreply.github.com>
2026-05-06 00:05:37 +0300

    new camera

--- assets/enteties/player/camera_3d.gd
+++ assets/enteties/player/camera_3d.gd
@@ -1,15 +1,27 @@
 extends Camera3D
 
+@export var player_path: NodePath
+@export var normal_offset := Vector3(0, 0, -5)
+@export var follow_smooth: float = 5.0
+@export var catch_up_smooth: float = 2.0
+
+var player: Node3D
+var current_offset: Vector3
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
+func _ready():
+	player = get_node(player_path)
+	current_offset = normal_offset
+	global_position = player.global_position + current_offset
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	$".".global_position.x = clamp($".".global_position.x, -8, 20)
-	$".".global_position.z = clamp($".".global_position.z, -4, -4)
-	$".".global_position.y = clamp($".".global_position.y, -5, 2.5)
+func _physics_process(delta):
+	if player == null:
+		print("PIZDEC")
+		return
+
+	global_position = Vector3(
+		lerpf(global_position.x, player.global_position.x + current_offset.x, catch_up_smooth * delta),
+		lerpf(global_position.y, player.global_position.y + current_offset.y, catch_up_smooth * delta),
+		player.global_position.z + current_offset.z
+	)
