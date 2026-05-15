81b343bca3634ef9de7348af926ef8be27880d4b
unknown <ladejnovmatvej@mail.ru>
2026-05-15 23:12:26 +0300

    Искры при ударе по врагу

--- assets/enteties/player/combat_component.gd
+++ assets/enteties/player/combat_component.gd
@@ -1,4 +1,7 @@
 extends Node
+
+@export var hit_sparks_scene: PackedScene
+
 @onready var area_dmg: Area3D = $Area_dmg
 @onready var cooldown_timer: Timer = $Timers/Attack_CoolDown_Timer
 
@@ -11,6 +14,7 @@ signal attack_finished
 func attack():
 	if not can_attack:
 		return
+
 	can_attack = false
 	attack_started.emit()
 	attack_action()
@@ -18,13 +22,33 @@ func attack():
 
 func attack_action():
 	var overlapping_bodies = area_dmg.get_overlapping_bodies()
+
 	for body in overlapping_bodies:
-		if body.has_method("take_dmg") and body.type == "enemy":
+		if body.has_method("take_dmg") and "type" in body and body.type == "enemy":
 			body.take_dmg(dmg)
+			spawn_hit_sparks(body)
+
+func spawn_hit_sparks(enemy: Node3D) -> void:
+	if hit_sparks_scene == null:
+		return
+
+	var sparks = hit_sparks_scene.instantiate()
+	get_tree().current_scene.add_child(sparks)
+
+	# Позиция между игроком и врагом
+	var hit_pos = owner.global_position.lerp(enemy.global_position, 0.5)
+
+	# Небольшое смещение вверх
+	hit_pos.y += 0.0
+
+	# Для 2.5D
+	hit_pos.z = 0
+
+	sparks.global_position = hit_pos
 
 func _on_attack_cool_down_timer_timeout():
 	can_attack = true
 	attack_finished.emit()
-	
+
 func _ready():
 	cooldown_timer.timeout.connect(_on_attack_cool_down_timer_timeout)
