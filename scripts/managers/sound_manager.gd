b6ec5bc82d3f080684e2938462a91ae899e058b4
iGore <glin4ak.igor@gmail.com>
2026-05-15 22:36:06 +0300

    rework of steamPipe? edits of SoundManager

--- scripts/managers/sound_manager.gd
+++ scripts/managers/sound_manager.gd
@@ -9,6 +9,10 @@ var playlist: Array = [
 	"res://assets/audios/music/ambient1.wav"
 ]
 
+var steam_pipe: Array = [
+	"res://assets/audios/sfx/steam.wav"
+]
+
 var current_track_index: int = 0
 @export var music_loop_delay: float = 3.0 # Пауза в секундах
 @export var fade_duration: float = 1.5   # Плавное появление (сек)
@@ -60,3 +64,36 @@ func _on_bgm_finished():
 func _on_delay_timer_timeout():
 	# Пауза закончилась — играем следующий трек
 	_play_current_track()
+
+# Универсальная функция: принимает либо String ("res://..."), либо Array ["res://...", ...]
+func play_sfx(sound_input, volume_db: float = 0.0):
+	var final_path: String = ""
+
+	# Проверяем: нам дали массив или одну строку?
+	if typeof(sound_input) == TYPE_ARRAY:
+		if sound_input.is_empty():
+			push_warning("SoundManager: Попытка проиграть пустой массив звуков!")
+			return
+		# Выбираем случайный звук из массива
+		final_path = sound_input[randi() % sound_input.size()]
+	elif typeof(sound_input) == TYPE_STRING:
+		final_path = sound_input
+	else:
+		push_error("SoundManager: Неверный тип данных для звука!")
+		return
+
+	# Создаем временный плеер
+	var sfx_player = AudioStreamPlayer.new()
+	add_child(sfx_player)
+	
+	var sound = load(final_path)
+	if sound:
+		sfx_player.stream = sound
+		sfx_player.bus = "SFX" # Убедись, что такая шина есть, или смени на "Master"
+		sfx_player.volume_db = volume_db
+		sfx_player.play()
+		
+		# Удаляем плеер из памяти, когда звук доиграет
+		sfx_player.finished.connect(sfx_player.queue_free)
+	else:
+		push_error("SoundManager: Файл не найден по пути: " + final_path)
